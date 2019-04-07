903b873a64281a9b93e9de33a508280959a3f92e
GCWoodgit config --global user.name GCWoodgit config --global user.email angelgabe101@gmail.com <angelgabe101@gmail.com>
2019-04-07 11:04:43 -0400

    done with weather file

--- weather/src/index.js
+++ weather/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 class App extends React.Component {
     constructor(props){
         super(props);
-
         this.state = { lat: 40, errorMessage: ''};
+    }
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 this.setState({
@@ -15,28 +17,28 @@ class App extends React.Component {
             (err) => {this.setState({errorMessage: err.message})}
         );
     }
-    componentDidMount(){
-        console.log('Component rendered');
-    }
     componentDidUpdate(){
         console.log('UPDATEED');
     }
-    render() {
+    renderContent(){
         if(this.state.errorMessage && !this.state.lat){
             return <div>{this.state.errorMessage}</div>;
-        }else if(!this.state.errorMessage && this.state.lat){
+        }
+        if(!this.state.errorMessage && this.state.lat){
             return (
-                <div>
-                    Latitude: {this.state.lat}
-                </div>
-            );
-        }else{
-            return(
-                <div>Loading...</div>
+                <SeasonDisplay lat={this.state.lat}></SeasonDisplay>
             );
         }
-        
-
+        return(
+            <Spinner message="Please accept location request"/>
+        );
+    }
+    render() {
+        return (
+            <div className="border black">
+                {this.renderContent()}
+            </div>
+        )
     }
 }
 
