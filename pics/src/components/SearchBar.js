dcafcd0f8f94d372a91024e316fb36e07a6a0477
GCWoodgit config --global user.name GCWoodgit config --global user.email angelgabe101@gmail.com <angelgabe101@gmail.com>
2019-04-12 20:08:11 -0400

    Event Handlers -- uncontrolled

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -5,13 +5,17 @@ class SearchBar extends React.Component {
     // construct(props){
     //     super(props);
     // }
+    onInputChange(event){
+        console.log(event);
+    }
     render(){
         return (
-            <div>
-                <form>
-                    <input type="text">
-                        
-                    </input>
+            <div className="ui segment">
+                <form className="ui form">
+                        <div className="field">
+                            <label>Image Search</label>
+                            <input type="text" onChange={this.onInputChange} onClick={() => console.log("Input Clicked")}/>
+                        </div>
                 </form>
             </div>
         );
